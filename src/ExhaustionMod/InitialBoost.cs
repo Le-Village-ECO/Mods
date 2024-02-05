65a5c896b521ef4a0e278a99dd9d3e18c3f861f3
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-02-05 23:21:11 +0100

    Exhaustion - divers corrections

--- src/ExhaustionMod/InitialBoost.cs
+++ src/ExhaustionMod/InitialBoost.cs
@@ -12,17 +12,18 @@ using Eco.Shared.Utils;
 using Eco.Gameplay.Systems;
 using Eco.Gameplay.Systems.Messaging.Chat.Commands;
 using System.Text;
+using System;
 
 namespace Village.Eco.Mods.ExhaustionMod
 {
     [ChatCommandHandler]
     public class InitialBoost : IModKitPlugin, IInitializablePlugin
     {
-        public static float ExhaustionAfterHour => BalanceConfig.Obj?.ExhaustionAfterHours ?? 0; //Nb heures par jour avant épuisement (Fichier config)
+        public static TimeSpan ExhaustionAfterHour => TimeSpan.FromHours(BalanceConfig.Obj?.ExhaustionAfterHours ?? 0); //Nb heures par jour avant épuisement (Fichier config)
 
-        public int currentWorldDay = (int)WorldTime.Day; //On ne garde que la partie entière de la valeur du jour du monde
+        public static double CurrentWorldDay => Math.Floor(WorldTime.Day); //On ne garde que la partie entière de la valeur du jour du monde
 
-        public float Calcul => ExhaustionAfterHour * currentWorldDay;
+        public static TimeSpan Calcul => ExhaustionAfterHour * CurrentWorldDay;
 
         [ChatCommand("ExhaustionMod commands")]
         public static void ExhaustionMod() { }
@@ -32,11 +33,9 @@ namespace Village.Eco.Mods.ExhaustionMod
         {
             StringBuilder sb = new();
 
-            int Calcul = (int)(BalanceConfig.Obj.ExhaustionAfterHours * WorldTime.Day); //On ne garde que la partie entière du jour
-
             sb.AppendLine($"La configuration est : {BalanceConfig.Obj.ExhaustionAfterHours} ");
-            sb.AppendLine($"Le jour du serveur est : {(int)WorldTime.Day} ");
-            sb.AppendLine($"Calcul : {Calcul} ");
+            sb.AppendLine($"Le jour du serveur est : {CurrentWorldDay} ");
+            sb.AppendLine($"Calcul : {Calcul.TotalHours} heures");
 
             user.Player.InfoBoxLocStr($"{sb}");
         }
@@ -46,7 +45,7 @@ namespace Village.Eco.Mods.ExhaustionMod
             //UserManager.OnUserLoggedIn.Add(user => { user.ExhaustionMonitor.Energize(Calcul); }); //Test OK
 
             // Sur l'event de 1ère connexion du joueur, on ajoute un nombre d'heure d'énergie basé sur la calcul
-            UserManager.NewUserJoinedEvent.Add(user => { user.ExhaustionMonitor.Energize(Calcul); } );  //a tester
+            UserManager.NewUserJoinedEvent.Add(user => { user.ExhaustionMonitor.Energize(Convert.ToSingle(Calcul.TotalHours)); } );  //a tester
 
         }
         public string GetStatus() => string.Empty;
