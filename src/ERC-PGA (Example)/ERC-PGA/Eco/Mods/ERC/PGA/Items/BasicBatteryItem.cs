9719633ecb47f8ed1bfd17dee5a3a5a27826b657
DelsantiV <vincent.delsanti@gmail.com>
2024-02-06 17:52:42 +0100

    Ajout du mod exemple ERC-PGA (décompilé)

---
+++ src/ERC-PGA (Example)/ERC-PGA/Eco/Mods/ERC/PGA/Items/BasicBatteryItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Eco.Gameplay.Items;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+
+namespace Eco.Mods.ERC.PGA.Items
+{
+	// Token: 0x02000009 RID: 9
+	[Serialized]
+	[LocDisplayName("Basic Battery")]
+	[LocDescription("Used for powering handheld electric tools, sadly not for long time")]
+	[MaxStackSize(25)]
+	[Weight(25)]
+	internal class BasicBatteryItem : Item
+	{
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x0600000D RID: 13 RVA: 0x00002361 File Offset: 0x00000561
+		public override LocString DisplayNamePlural
+		{
+			get
+			{
+				return Localizer.DoStr("Basic Batteries");
+			}
+		}
+	}
+}
---
+++ src/ERC-PGA (Example)/ERC-PGA/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyCompany("Eco Russian Community")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Eco Russian Community 2022")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyInformationalVersion("1.2.1+78f7f869842e3693f8f337756fbb3f4e0d7edccd")]
+[assembly: AssemblyProduct("ERC-PGA")]
+[assembly: AssemblyTitle("ERC-PGA")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/Eco-Russian-Community/")]
+[module: RefSafetyRules(11)]
