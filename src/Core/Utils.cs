adf7df78e09ed826229abc093413fa178517f438
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-03-02 19:14:03 +0100

    Ajout class utilitaire dans le core

---
+++ src/Core/Utils.cs
@@ -0,0 +1,21 @@
+﻿using Eco.Core.Utils.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village.Eco.Mods.Core
+{
+    public static class Utils
+    {
+        // Permet de log la pile d'appels pour analyser et debuguer
+        // Il suffit d'appeler LogWithStackTrace
+        public static void LogWithStackTrace() 
+        {
+            var stack = Environment.StackTrace;
+            var log = NLogManager.GetLogWriter("LeVillageMods");
+            log.Write($"Pile d'appels {stack}");
+        }
+    }
+}
