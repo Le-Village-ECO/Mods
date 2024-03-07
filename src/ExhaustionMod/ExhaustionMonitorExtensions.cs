23776b34c20bf500b61c74be7b525822f975e4df
Bejarid <157166777+Bejarid@users.noreply.github.com>
2024-03-07 02:13:55 +0100

    Ajout de ExhaustionMonitorExtensions pour ajouter de l'énergie à un joueur

---
+++ src/ExhaustionMod/ExhaustionMonitorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Reflection;
+using Eco.Gameplay.Players;
+using Eco.Shared.Utils;
+
+namespace ExhaustionMod
+{
+    //Le village
+    public static class ExhaustionMonitorExtensions
+    {
+        public static void AddEnergy(this ExhaustionMonitor monitor, float hours)
+        {
+            var exhaustionMonitorFields = typeof(ExhaustionMonitor).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            var extraEnergySecondsFieldInfo = exhaustionMonitorFields.First(p => p.Name == "ChangeSets");
+            var extraEnergySecondsFieldValue = (double)extraEnergySecondsFieldInfo.GetValue(monitor)!;
+
+            monitor.Energize(hours);
+
+            var extendedExtraEnergySeconds = extraEnergySecondsFieldValue + TimeUtil.HoursToSeconds(hours);
+            extraEnergySecondsFieldInfo.SetValue(monitor, extendedExtraEnergySeconds);
+
+            monitor.Tick();
+        }
+    }
+}
