272e95cafe7d2446fab95b6fdd9cb3426869144b
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-03-08 15:52:56 +0100

    Ajout 6h weekend

--- src/ExhaustionMod/InitialBoost.cs
+++ src/ExhaustionMod/InitialBoost.cs
@@ -1,14 +1,18 @@
 ﻿// Le Village
-// L'objectif est de donner l'énergie accumulable depuis le lancement du cycle (jour 1) au joueur
-// lors de sa 1ère connexion au serveur
+// Lors de la 1ère connexion : Donner le temps accumulable depuis le lancement du cycle (jour 1)
+// Lors d'une connexion : Si pas de flag, donner un bonus de 6h (bidouille pour donner 6h en weekend)
 
+using Eco.Core;
 using Eco.Core.Plugins.Interfaces;
 using Eco.Core.Utils;
+using Eco.Core.Utils.Logging;
 using Eco.Gameplay.Players;
 using Eco.Gameplay.Systems;
 using Eco.Gameplay.Systems.Messaging.Chat.Commands;
 using Eco.Simulation.Time;
+using ExhaustionMod;
 using System;
+using Village.Eco.Mods.Core;
 
 namespace Village.Eco.Mods.ExhaustionMod
 {
@@ -23,11 +27,35 @@ namespace Village.Eco.Mods.ExhaustionMod
 
         public void Initialize(TimedTask timer)
         {
-            //UserManager.OnUserLoggedIn.Add(user => { user.ExhaustionMonitor.Energize(Calcul); }); //Test OK
+
+            //Donne un boost de X heure(s) en fonction d'une variable bool
+            UserManager.OnUserLoggedIn.Add(user => 
+            {
+                //Recuperation des donnees du joueur
+                var plugin = PluginManager.GetPlugin<PlayersDataPlugin>();
+                var playerData = plugin.GetPlayerDataOrDefault(user.Player);
+
+                int hours = 6;
+                //Si jamais boosté
+                if (!playerData.BoostWE)
+                {
+                    //user.ExhaustionMonitor.Energize(hours);
+                    user.ExhaustionMonitor.AddEnergy(hours);
+
+                    //variable joueur TRUE
+                    playerData.BoostWE = true;
+                    plugin.AddOrSetPlayerData(user.Player, playerData);
+                    
+                    //Log
+                    var log = NLogManager.GetLogWriter("LeVillageMods");
+                    log.Write($"Le joueur **{user.Player.DisplayName}** a reçu son boost WE de {hours} heure(s).");
+                }
+            });
 
             // Sur l'event de 1ère connexion du joueur, on ajoute un nombre d'heure d'énergie basé sur la calcul
             UserManager.NewUserJoinedEvent.Add(user => { user.ExhaustionMonitor.Energize(Convert.ToSingle(Calcul.TotalHours)); } );
         }
+
         public string GetStatus() => string.Empty;
         public string GetCategory() => "LeVillageMods";
     }
