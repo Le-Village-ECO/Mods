6099425f0f1909817bd8bd44e99937e7306e4506
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-03-16 20:26:54 +0100

    Ajout outils récupération liste de joueurs

--- src/Core/Utils.cs
+++ src/Core/Utils.cs
@@ -1,9 +1,8 @@
 ﻿using Eco.Core.Utils.Logging;
+using Eco.Gameplay.Players;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Village.Eco.Mods.Core
 {
@@ -17,5 +16,22 @@ namespace Village.Eco.Mods.Core
             var log = NLogManager.GetLogWriter("LeVillageMods");
             log.Write($"Pile d'appels {stack}");
         }
+    }
+    public class PlayerUtils
+    {
+        // Permet d'obtenir la liste de tous les joueurs du serveur
+        // Exemple d'utlisation :
+        // foreach (var user in PlayerUtils.Users)
+        // {
+        //     user.Mailbox.Add(mailMessage, !user.IsOnline);
+        // }
+        public static List<User> AllUsers => UserManager.Users.ToList();
+
+        // Permet d'obtenir la liste des joueurs en ligne
+        //public static List<User> OnlineUsers => UserManager.OnlineUsers.ToList(); //pb de package...
+        public static List<User> OnlineUsers => UserManager.Users.Where(u => u.IsOnline).ToList();
+        
+        // Permet d'avoir une liste de joueurs épuisés
+        public static List<User> ExhaustedUsers => UserManager.Users.Where(u => u.ExhaustionMonitor.IsExhausted).ToList();
     }
 }
