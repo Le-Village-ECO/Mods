03e54ff2184a0a2436435db30ed63cb794a961a6
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-03-19 19:01:21 +0100

    Init Diet

---
+++ src/Nutrition/Diet.cs
@@ -0,0 +1,57 @@
+﻿using Eco.Core.Items;
+using Eco.Gameplay.DynamicValues;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Skills;
+using Eco.Mods.TechTree;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+
+namespace Village.Eco.Mods.Nutrition
+{
+    [Serialized]
+    [LocDisplayName("Diet")]
+    [LocDescription("Bien manger")]
+    [Ecopedia("Professions", "Survivalist", createAsSubPage: true)]
+    [RequiresSkill(typeof(SurvivalistSkill), 0), Tag("Survivalist Specialty"), Tier(1)]
+    [Tag("Specialty")]
+    [Tag("Teachable")]
+    public partial class Diet : Skill
+    {
+        public override void OnReset(User user) { this.OnLevelChanged(user); }
+        public override void OnLevelUp(User user) { this.OnLevelChanged(user); }
+        private void OnLevelChanged(User user)
+        {
+            user.Stomach.ChangedMaxCalories();
+            user.ChangedCarryWeight();
+        }
+
+        public static MultiplicativeStrategy MultiplicativeStrategy =
+            new MultiplicativeStrategy(new float[] {
+                1,
+                1 - 0.05f,
+                1 - 0.1f,
+                1 - 0.15f,
+                1 - 0.2f,
+                1 - 0.25f,
+                1 - 0.25f,
+                1 - 0.25f,
+            });
+        public override MultiplicativeStrategy MultiStrategy => MultiplicativeStrategy;
+
+        public static AdditiveStrategy AdditiveStrategy =
+            new AdditiveStrategy(new float[] {
+                0,
+                0,
+                250,
+                500,
+                1000,
+                1500,
+                2000,
+                2500,
+            });
+        public override AdditiveStrategy AddStrategy => AdditiveStrategy;
+        public override int MaxLevel { get { return 7; } }
+        public override int Tier { get { return 1; } }
+    }
+}
---
+++ src/Nutrition/DietCommands.cs
@@ -0,0 +1,48 @@
+﻿using Eco.Core.Utils.Logging;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Systems.Messaging.Chat.Commands;
+using Eco.Gameplay.Systems.Messaging.Notifications;
+using Eco.Gameplay.UI;
+using Eco.Gameplay.Utils;
+using Eco.Shared.Localization;
+using Village.Eco.Mods.Core;
+
+namespace Village.Eco.Mods.Nutrition
+{
+    [ChatCommandHandler]
+    public static partial class DietCommands
+    {
+        [ChatCommand("Diet", ChatAuthorizationLevel.Admin)]
+        public static void LVDiet() { }
+
+        [ChatSubCommand("LVDiet", "test", ChatAuthorizationLevel.Admin)]
+        public static void Test1(User user, string foodName)
+        {
+            var foodItem = CommandsUtil.ClosestMatchingItem<FoodItem>(user, foodName);
+            string test = user.Stomach.TasteBuds.GetFoodTaste(foodItem);
+            user.Player.Msg(Localizer.Format($"{foodName} = {foodItem} = {test}"));
+
+                foreach (var item in PlayerUtils.Food)
+                {
+                    user.Player.MsgLocStr($"loop : {item}");
+                }
+                //return true;
+
+
+        }
+
+        [ChatSubCommand("LVDiet", "test2", ChatAuthorizationLevel.Admin)]
+        public static void Test2(User user)
+        {
+            user.Player.Msg(Localizer.Format($"coucou"));
+        }
+    }
+
+    public class MethodUtils
+    {
+        public static List<Item> Food => FoodItem.AllItemsExceptHidden.ToList();
+        public static List<Item> Tool => Item.AllItemsExceptHidden.Where(i => i.IsTool).ToList();
+    }
+
+}
