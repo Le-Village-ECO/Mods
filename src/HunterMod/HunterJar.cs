583566e3ae15f1d4de8e27816d471ec9dcf622b6
DarkNoTv <140497527+DarkNoTv@users.noreply.github.com>
2024-04-12 12:36:28 +0200

    1er Jet Jarre - Toutes carcasses viande sauf bison - Equivalent d'un coffre pour le moment

---
+++ src/HunterMod/HunterJar.cs
@@ -0,0 +1,143 @@
+﻿// Le VIllage - Jarre de conservations des carcasses
+
+namespace Eco.Mods.TechTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
+    using Eco.Gameplay.Minimap;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Occupancy;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Shared;
+    using Eco.Shared.Math;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
+    using Eco.Shared.Networking;
+    using Eco.Gameplay.Pipes;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Housing.PropertyValues;
+    using Eco.Gameplay.Civics.Objects;
+    using Eco.Gameplay.Settlements;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Core.Controller;
+    using Eco.Core.Utils;
+    using Eco.Gameplay.Components.Storage;
+    using Eco.Gameplay.Items.Recipes;
+
+    [Serialized]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(LinkComponent))]
+    [RequireComponent(typeof(PublicStorageComponent))]
+    [RequireComponent(typeof(ForSaleComponent))]
+    [RequireComponent(typeof(RoomRequirementsComponent))]
+    [RequireRoomContainment]
+    [RequireRoomVolume(8)]
+    [Tag("Usable")]
+    [Ecopedia("Crafted Objects", "Storage", subPageName: "Jarre du chasseur Item")]
+    public partial class HunterJarObject : WorldObject, IRepresentsItem
+    {
+        public virtual Type RepresentedItemType => typeof(HunterJarItem);
+        public override LocString DisplayName => Localizer.DoStr("Jarre du chasseur");
+        public override TableTextureMode TableTexture => TableTextureMode.Wood;
+
+        protected override void Initialize()
+        {
+            this.ModsPreInitialize();
+            var storage = this.GetComponent<PublicStorageComponent>();
+            storage.Initialize(4);
+            storage.Storage.AddInvRestriction(new TagRestriction("MediumCarcass","SmallCarcass","TinyCarcass"));// Carcass only A MODIFIER 
+            this.ModsPostInitialize();
+        }
+
+        /// <summary>Hook for mods to customize WorldObject before initialization. You can change housing values here.</summary>
+        partial void ModsPreInitialize();
+        /// <summary>Hook for mods to customize WorldObject after initialization.</summary>
+        partial void ModsPostInitialize();
+    }
+
+    [Serialized]
+    [LocDisplayName("Jarre du chasseur")]
+    [LocDescription("A container you can store items in.")]
+    [Ecopedia("Crafted Objects", "Storage", createAsSubPage: true)]
+    [Weight(1000)] // Defines how heavy HunterJar is.
+    public partial class HunterJarItem : WorldObjectItem<HunterJarObject>
+    {
+
+    }
+
+    /// <summary>
+    /// <para>Server side recipe definition for "HunterJar".</para>
+    /// <para>More information about RecipeFamily objects can be found at https://docs.play.eco/api/server/eco.gameplay/Eco.Gameplay.Items.RecipeFamily.html</para>
+    /// </summary>
+    /// <remarks>
+    /// This is an auto-generated class. Don't modify it! All your changes will be wiped with next update! Use Mods* partial methods instead for customization. 
+    /// If you wish to modify this class, please create a new partial class or follow the instructions in the "UserCode" folder to override the entire file.
+    /// </remarks>
+    [Ecopedia("Crafted Objects", "Storage", subPageName: "Jarre du chasseur Item")]
+    public partial class HunterJarRecipe : RecipeFamily
+    {
+        public HunterJarRecipe()
+        {
+            var recipe = new Recipe();
+            recipe.Init(
+                name: "HunterJar",  //noloc
+                displayName: Localizer.DoStr("Jarre du chasseur"),
+
+                // Defines the ingredients needed to craft this recipe. An ingredient items takes the following inputs
+                // type of the item, the amount of the item, the skill required, and the talent used.
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement("Wood", 10,typeof(Skill)), //noloc
+                },
+
+                // Define our recipe output items.
+                // For every output item there needs to be one CraftingElement entry with the type of the final item and the amount
+                // to create.
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<HunterJarItem>()
+                });
+            this.Recipes = new List<Recipe> { recipe };
+
+            // Defines the amount of labor required and the required skill to add labor
+            this.LaborInCalories = CreateLaborInCaloriesValue(25);
+
+            // Defines our crafting time for the recipe
+            this.CraftMinutes = CreateCraftTimeValue(1);
+
+            // Perform pre/post initialization for user mods and initialize our recipe instance with the display name "Jarre du chasseur"
+            this.ModsPreInitialize();
+            this.Initialize(displayText: Localizer.DoStr("Jarre du chasseur"), recipeType: typeof(HunterJarRecipe));
+            this.ModsPostInitialize();
+
+            // Register our RecipeFamily instance with the crafting system so it can be crafted.
+            CraftingComponent.AddRecipe(tableType: typeof(WorkbenchObject), recipe: this);
+        }
+
+        /// <summary>Hook for mods to customize RecipeFamily before initialization. You can change recipes, xp, labor, time here.</summary>
+        partial void ModsPreInitialize();
+
+        /// <summary>Hook for mods to customize RecipeFamily after initialization, but before registration. You can change skill requirements here.</summary>
+        partial void ModsPostInitialize();
+    }
+}
