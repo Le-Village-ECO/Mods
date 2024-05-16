c30fccfff23e657366f421a97e92a64b16f510b9
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-05-16 22:15:08 +0200

    Initialisation du skill Housing

---
+++ src/HousingMod/HousingCommands.cs
@@ -0,0 +1,18 @@
+﻿using Eco.Gameplay.Players;
+using Eco.Gameplay.Systems.Messaging.Chat.Commands;
+
+namespace Village.Eco.Mods.HousingMod
+{
+    [ChatCommandHandler]
+    public static partial class DietCommands
+    {
+        [ChatCommand("LVHousing", ChatAuthorizationLevel.Admin)]
+        public static void LVHousing() { }
+
+        [ChatSubCommand("LVHousing", "Test1", ChatAuthorizationLevel.Admin)]
+        public static void Test1(User user)
+        {
+
+        }
+    }
+}
---
+++ src/HousingMod/HousingPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Eco.Core.Plugins.Interfaces;
+using Eco.Gameplay.Players;
+using Eco.Shared.Localization;
+
+namespace Village.Eco.Mods.HousingMod
+{
+    public class HousingPlugin : IModInit
+    {
+        public static void PostInitialize()
+        {
+            UserManager.NewUserJoinedEvent.Add(user =>
+            {
+                var skill = user.Skillset.GetOrAddSkill(typeof(HousingSkill));
+                skill.ForceSetLevel(user, 1);
+            });
+        }
+    }
+}
