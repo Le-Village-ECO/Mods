5696a97c0ab187deaec6a40423f460b904f1d1cd
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-06-20 17:00:30 +0200

    Template de WorldObject

---
+++ src/Templates2/WorldObject.cs
@@ -0,0 +1,35 @@
+﻿using Eco.Core.Controller;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Occupancy;
+using Eco.Gameplay.Systems.NewTooltip;
+using Eco.Shared.Items;
+using Eco.Shared.Localization;
+using Eco.Shared.Math;
+using Eco.Shared.Serialization;
+
+namespace Village.Eco.Mods.OilField
+{
+    [Serialized]
+    public partial class TemplateObject : WorldObject, IRepresentsItem
+    {
+        public virtual Type RepresentedItemType => typeof(TemplateItem);
+        public override LocString DisplayName => Localizer.DoStr("Template Object");
+        public override TableTextureMode TableTexture => TableTextureMode.Wood;
+        protected override void Initialize()
+        {
+            this.ModsPreInitialize();
+            this.ModsPostInitialize();
+        }
+        partial void ModsPreInitialize();
+        partial void ModsPostInitialize();
+    }
+
+    [Serialized]
+    [LocDisplayName("Template Object")]
+    public partial class TemplateItem : WorldObjectItem<TemplateObject>, IPersistentData
+    {
+        protected override OccupancyContext GetOccupancyContext => new SideAttachedContext(0 | DirectionAxisFlags.Down, WorldObject.GetOccupancyInfo(this.WorldObjectType));
+        [Serialized, SyncToView, NewTooltipChildren(CacheAs.Instance, flags: TTFlags.AllowNonControllerTypeForChildren)] public object PersistentData { get; set; }
+    }
+}
