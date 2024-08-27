1ebd974644f9aeead02403e147fded10d9835561
Apocalis_31 <127745096+Apocalis-31@users.noreply.github.com>
2024-08-27 18:58:25 +0200

    ajout entrée de mine V2

---
+++ src/CosmeticMod/TruckMineGate.cs
@@ -0,0 +1,139 @@
+﻿namespace Eco.Mods.TechTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
+    using Eco.Gameplay.Minimap;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Occupancy;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Shared;
+    using Eco.Shared.Math;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
+    using Eco.Shared.Networking;
+    using Eco.Gameplay.Pipes;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Housing.PropertyValues;
+    using Eco.Gameplay.Civics.Objects;
+    using Eco.Gameplay.Settlements;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Core.Controller;
+    using Eco.Core.Utils;
+    using Eco.Gameplay.Components.Storage;
+    using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
+    using Eco.Gameplay.Items.Recipes;
+
+    [Serialized]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(OccupancyRequirementComponent))]
+    [RequireComponent(typeof(ForSaleComponent))]
+    [RequireComponent(typeof(PaintableComponent))] //TODO: Fix not all the object have this component
+    [Tag("Décoration")]
+    [Ecopedia("Decoration", "Décoration de mine", subPageName: "Entrée de mine pour camion")]
+    public partial class TruckMineGateObject : WorldObject, IRepresentsItem
+    {
+
+        public virtual Type RepresentedItemType => typeof(AdornedAshlarShaleTableItem);
+        public override LocString DisplayName => Localizer.DoStr("Entrée de mine pour camion");
+        public override TableTextureMode TableTexture => TableTextureMode.Stone;
+
+        static TruckMineGateObject()
+
+        {
+            WorldObject.AddOccupancy<TruckMineGateObject>(new List<BlockOccupancy>(){
+            new BlockOccupancy(new Vector3i(0, 0, 0)),
+            new BlockOccupancy(new Vector3i(0, 1, 0)),
+            new BlockOccupancy(new Vector3i(0, 2, 0)),
+            new BlockOccupancy(new Vector3i(1, 0, 0)),
+            new BlockOccupancy(new Vector3i(1, 1, 0)),
+            new BlockOccupancy(new Vector3i(1, 2, 0)),
+            });
+
+
+        }
+
+
+
+        /// <summary>Hook for mods to customize WorldObject before initialization. You can change housing values here.</summary>
+        partial void ModsPreInitialize();
+        /// <summary>Hook for mods to customize WorldObject after initialization.</summary>
+        partial void ModsPostInitialize();
+    }
+
+    [Serialized]
+    [LocDisplayName("Entrée de mine pour camion")]
+    [LocDescription("Entrée de mine pour camion taille 3 de large et 3 de haut.")]
+    [Ecopedia("Decoration", "Décoration de mine", createAsSubPage: true)]
+    [Tag("Décoration")]
+    [Weight(2000)] // Defines how heavy AdornedAshlarShaleTable is.
+    public partial class TruckMineGateItem : WorldObjectItem<TruckMineGateObject>
+    {
+        protected override OccupancyContext GetOccupancyContext => new SideAttachedContext(0 | DirectionAxisFlags.Down, WorldObject.GetOccupancyInfo(this.WorldObjectType));
+
+
+
+    }
+
+    /// <summary>
+    /// <para>Server side recipe definition for "AdornedAshlarShaleTable".</para>
+    /// <para>More information about RecipeFamily objects can be found at https://docs.play.eco/api/server/eco.gameplay/Eco.Gameplay.Items.RecipeFamily.html</para>
+    /// </summary>
+    /// <remarks>
+    /// This is an auto-generated class. Don't modify it! All your changes will be wiped with next update! Use Mods* partial methods instead for customization. 
+    /// If you wish to modify this class, please create a new partial class or follow the instructions in the "UserCode" folder to override the entire file.
+    /// </remarks>
+    [RequiresSkill(typeof(CarpentrySkill), 2)]
+    [ForceCreateView]
+    [Ecopedia("Decoration", "Décoration de mine", subPageName: "Entrée de mine pour camion")]
+    public partial class TruckMineGateRecipe : Recipe
+    {
+        public TruckMineGateRecipe()
+        {
+            this.Init(
+                name: "Entrée de mine pour camion",  //noloc
+                displayName: Localizer.DoStr("Entrée de mine pour camion"),
+
+                // Defines the ingredients needed to craft this recipe. An ingredient items takes the following inputs
+                // type of the item, the amount of the item, the skill required, and the talent used.
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement("Wood", 16, typeof(CarpenterSkill), typeof(CarpentryLavishResourcesTalent)),
+                },
+
+                // Define our recipe output items.
+                // For every output item there needs to be one CraftingElement entry with the type of the final item and the amount
+                // to create.
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<TruckMineGateItem>()
+                });
+            // Perform post initialization steps for user mods and initialize our recipe instance as a tag product with the crafting system
+            this.ModsPostInitialize();
+            CraftingComponent.AddTagProduct(typeof(CarpentryTableObject), typeof(TruckMineGateRecipe), this);
+        }
+
+
+        /// <summary>Hook for mods to customize RecipeFamily after initialization, but before registration. You can change skill requirements here.</summary>
+        partial void ModsPostInitialize();
+    }
+}
