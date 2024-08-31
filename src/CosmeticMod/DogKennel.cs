8d3299fd6a98cf6bac89d504bc63367582bff2df
Apocalis_31 <127745096+Apocalis-31@users.noreply.github.com>
2024-08-31 12:05:46 +0200

    Maj unity tag décoration Ajout Dogkennel refont icon

---
+++ src/CosmeticMod/DogKennel.cs
@@ -0,0 +1,141 @@
+﻿namespace Eco.Mods.TechTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
+    using Eco.Gameplay.Minimap;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Occupancy;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Shared;
+    using Eco.Shared.Math;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
+    using Eco.Shared.Networking;
+    using Eco.Gameplay.Pipes;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Housing.PropertyValues;
+    using Eco.Gameplay.Civics.Objects;
+    using Eco.Gameplay.Settlements;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Core.Controller;
+    using Eco.Core.Utils;
+    using Eco.Gameplay.Components.Storage;
+    using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
+    using Eco.Gameplay.Items.Recipes;
+
+    [Serialized]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(OccupancyRequirementComponent))]
+    [RequireComponent(typeof(ForSaleComponent))]
+    [RequireComponent(typeof(PaintableComponent))] //TODO: Fix not all the object have this component
+    [Tag("Decoration")]
+    [Ecopedia("Decoration", "Décoration standart", subPageName: "Niche pour chien")]
+    public partial class DogKennelObject : WorldObject, IRepresentsItem
+    {
+
+        public virtual Type RepresentedItemType => typeof(DogKennelItem);
+        public override LocString DisplayName => Localizer.DoStr("Niche pour chien");
+        public override TableTextureMode TableTexture => TableTextureMode.Stone;
+
+        static DogKennelObject()
+        {
+            var BlockOccupancyList = new List<BlockOccupancy>
+            {
+            // DogKennelObject
+            new BlockOccupancy(new Vector3i(0, 0, 0)),
+            new BlockOccupancy(new Vector3i(0, 0, 1)),
+            new BlockOccupancy(new Vector3i(1, 0, 0)),
+            new BlockOccupancy(new Vector3i(1, 0, 1)),
+
+            };
+
+            AddOccupancy<DogKennelObject>(BlockOccupancyList);
+
+        }
+
+
+
+
+        /// <summary>Hook for mods to customize WorldObject before initialization. You can change housing values here.</summary>
+        partial void ModsPreInitialize();
+        /// <summary>Hook for mods to customize WorldObject after initialization.</summary>
+        partial void ModsPostInitialize();
+    }
+
+    [Serialized]
+    [LocDisplayName("Niche pour chien")]
+    [LocDescription("Une petite niche pour chien ... ou pour celui qui veut.")]
+    [Ecopedia("Decoration", "Décoration standart", createAsSubPage: true)]
+    [Tag("Decoration")]
+    [Weight(2000)] // Defines how heavy AdornedAshlarShaleTable is.
+    public partial class DogKennelItem : WorldObjectItem<DogKennelObject>
+    {
+        protected override OccupancyContext GetOccupancyContext => new SideAttachedContext(0 | DirectionAxisFlags.Down, WorldObject.GetOccupancyInfo(this.WorldObjectType));
+
+
+
+    }
+
+    /// <summary>
+    /// <para>Server side recipe definition for "AdornedAshlarShaleTable".</para>
+    /// <para>More information about RecipeFamily objects can be found at https://docs.play.eco/api/server/eco.gameplay/Eco.Gameplay.Items.RecipeFamily.html</para>
+    /// </summary>
+    /// <remarks>
+    /// This is an auto-generated class. Don't modify it! All your changes will be wiped with next update! Use Mods* partial methods instead for customization. 
+    /// If you wish to modify this class, please create a new partial class or follow the instructions in the "UserCode" folder to override the entire file.
+    /// </remarks>
+    [RequiresSkill(typeof(CarpentrySkill), 2)]
+    [ForceCreateView]
+    [Ecopedia("Decoration", "Décoration standart", subPageName: "Niche pour chien")]
+    public partial class DogKennelRecipe : Recipe
+    {
+        public DogKennelRecipe()
+        {
+            this.Init(
+                name: "Niche pour chien",  //noloc
+                displayName: Localizer.DoStr("Niche pour chien"),
+
+                // Defines the ingredients needed to craft this recipe. An ingredient items takes the following inputs
+                // type of the item, the amount of the item, the skill required, and the talent used.
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement("Wood", 6, typeof(CarpenterSkill), typeof(CarpentryLavishResourcesTalent)),
+                },
+
+                // Define our recipe output items.
+                // For every output item there needs to be one CraftingElement entry with the type of the final item and the amount
+                // to create.
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<DogKennelItem>()
+                });
+            // Perform post initialization steps for user mods and initialize our recipe instance as a tag product with the crafting system
+            this.ModsPostInitialize();
+            CraftingComponent.AddTagProduct(typeof(CarpentryTableObject), typeof(DogKennelRecipe), this);
+        }
+
+
+        /// <summary>Hook for mods to customize RecipeFamily after initialization, but before registration. You can change skill requirements here.</summary>
+        partial void ModsPostInitialize();
+    }
+}
