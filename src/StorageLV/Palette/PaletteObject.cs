7661726d16b3d77289c891c0aecf6072c48a7472
plex68 <73914324+plex68@users.noreply.github.com>
2024-09-02 16:40:45 +0200

    Ajout

---
+++ src/StorageLV/Palette/PaletteObject.cs
@@ -0,0 +1,31 @@
+﻿namespace Eco.Mods.TechTree
+{
+    using Eco.Gameplay.Components.Storage;
+    using Eco.Gameplay.Objects;
+    using Eco.Shared.Math;
+    using Eco.Shared.SharedTypes;
+
+    [RequireComponent(typeof(PublicStorageComponent))]
+    [RequireComponent(typeof(StockpileComponent))]
+    [RequireComponent(typeof(WorldStockpileComponent))]
+    public partial class PaletteObject : WorldObject
+    {
+        public static readonly Vector3i DefaultDim = new Vector3i(2, 5, 2);
+        public override InteractionTargetPriority TargetPriority => InteractionTargetPriority.Medium;
+
+        protected override void OnCreatePostInitialize()
+        {
+            base.OnCreatePostInitialize();
+            StockpileComponent.ClearPlacementArea(this.Creator, this.Position3i, DefaultDim, this.Rotation);
+        }
+
+        protected override void PostInitialize()
+        {
+            base.PostInitialize();
+            
+            var storage = this.GetComponent<PublicStorageComponent>();
+            storage.Initialize(DefaultDim.x * DefaultDim.z);
+            storage.Storage.AddInvRestriction(new StockpileStackRestriction(DefaultDim.y)); // limit stack sizes to the y-height of the stockpile
+        }
+    }
+}
