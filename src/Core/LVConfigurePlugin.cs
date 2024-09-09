ce7dc2b2a047f78c668b2ccc7e13e78e4120c43a
Bejarid <bejarid@hotmail.com>
2024-09-09 20:27:20 +0200

    Ajout d'une méthode de sauvegarde au fichier de config Core

--- src/Core/LVConfigurePlugin.cs
+++ src/Core/LVConfigurePlugin.cs
@@ -10,13 +10,16 @@ namespace Village.Eco.Mods.Core
     [LocDisplayName("LV Configure Plugin")]
     public class LVConfigurePlugin : IModKitPlugin, IConfigurablePlugin
     {
+        private const string FILE_NAME = "LVConfigure";
+
         private static readonly PluginConfig<LVConfigureConfig> config; 
         public IPluginConfig PluginConfig => config;
         public static LVConfigureConfig Config => config.Config;
-        static LVConfigurePlugin() => config = new PluginConfig<LVConfigureConfig>("LVConfigure");
+        static LVConfigurePlugin() => config = new PluginConfig<LVConfigureConfig>(FILE_NAME);
         public ThreadSafeAction<object, string> ParamChanged { get; set; } = new ThreadSafeAction<object, string>();
         public object GetEditObject() => config.Config;
         public void OnEditObjectChanged(object o, string param) => this.SaveConfig();
+        public static void Save() => config.SaveAsAsync(FILE_NAME).Wait();
 
         public string GetCategory() => Localizer.DoStr("LeVillageMods");
         public override string ToString() => Localizer.DoStr("Configuration");
