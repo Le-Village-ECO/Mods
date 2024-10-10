af8236e6020fcecc581344482cda8bcc3924581c
Spouke-ECO <89523390+Spouke-ECO@users.noreply.github.com>
2024-10-10 13:56:43 +0200

    ajout tooltip

--- src/LVShared/UserCode/LVMods/VillageHebdo/VillageHebdo.cs
+++ src/LVShared/UserCode/LVMods/VillageHebdo/VillageHebdo.cs
@@ -1,20 +1,17 @@
-﻿using Eco.Core.Controller;
+﻿// Le Village - Village Hebdo est un journal d'information absolument pas objectif
+
 using Eco.Core.Items;
 using Eco.Gameplay.Items;
-using Eco.Gameplay.Objects;
-using Eco.Gameplay.Occupancy;
+using Eco.Gameplay.Players;
 using Eco.Gameplay.Systems.NewTooltip;
+using Eco.Gameplay.Systems.NewTooltip.TooltipLibraryFiles;
+using Eco.Gameplay.Systems.TextLinks;
 using Eco.Shared.Items;
 using Eco.Shared.Localization;
-using Eco.Shared.Math;
 using Eco.Shared.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LVShared.UserCode.LVMods.Plugins;
 
-namespace LVShared.UserCode.LVMods.LVHebdo
+namespace LVShared.UserCode.LVMods.VillageHebdo
 {
     [Serialized]
     [LocDisplayName("Le Village Hebdo")]
@@ -23,6 +20,18 @@ namespace LVShared.UserCode.LVMods.LVHebdo
     [Weight(50)]
     public partial class VillageHebdoItem : Item
     {
+        public override string OnUsed(Player player, ItemStack itemStack) 
+        {
+            WelcomePlugin.LastNews(player.User);
+            return base.OnUsed(player, itemStack);
+        }
+    }
 
+    [TooltipLibrary]
+    public static class VillageHebdoExt 
+    {
+        //[NewTooltip(CacheAs.Disabled, overrideType: typeof(VillageHebdoItem))]
+        [NewTooltip(CacheAs.Disabled)]
+        public static LocString VillageHebdoTooltip(this VillageHebdoItem type) => TextLoc.ControlsLoc($"Right-click to read {type.UILink()}.", "[", "]");
     }
 }
