0f99cd42b900d3abbb56f27fa155b4cf19230cb5
plex68 <73914324+plex68@users.noreply.github.com>
2024-10-11 09:58:05 +0200

    - Traduction TradeAssistant Francais

---
+++ src/TradeAssistant1-FRANCAIS/TradeAssistant-0.1.9.0-FRANCAIS/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using Eco.Gameplay.Components.Store;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Settlements;
+using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TradeAssistant
+{
+    public static class Extensions
+    {
+        public static UserConfig Config(this User user)
+        {
+            if (!TradeAssistantData.Obj.UserConfiguration.TryGetValue(user.Id, out var config))
+            {
+                config = new UserConfig();
+                TradeAssistantData.Obj.UserConfiguration.Add(user.Id, config);
+            }
+
+            return config;
+        }
+
+        public static void AddLoc(this List<LocString> list, FormattableString msg)
+        {
+            list.Add(Localizer.Do(msg));
+        }
+
+        public static float GetTax(this StoreComponent store)
+        {
+            var settlement = SettlementUtils.GetSettlementsAtPos(store.Parent.Position3i);
+            if (settlement == null) return 0;
+            return settlement.Sum(s => s.Taxes.GetSalesTax(store.Currency));
+        }
+
+        public static string ToStyledNum(this float number)
+        {
+            return Text.StyledNum(number);
+        }
+    }
+}
